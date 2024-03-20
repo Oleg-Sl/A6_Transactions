99fd2981b98427f5233aacebf277dbd407dcccbb
staunton <staunton@antoneo.myguest.virtualbox.org>
2024-03-20 22:45:37 +0700

    fix format

--- src/bst/self_balancing_binary_search_tree.h
+++ src/bst/self_balancing_binary_search_tree.h
@@ -1,151 +1,143 @@
 #ifndef TRANSACTIONS_BST_SELF_BALANCING_BINARY_SEARCH_TREE_H_
 #define TRANSACTIONS_BST_SELF_BALANCING_BINARY_SEARCH_TREE_H_
 
+#include <fstream>
+
 #include "../common/base_class.h"
 #include "../common/data.h"
-#include <fstream>
 
 namespace s21 {
 
-	enum Color {
-		Black,
-		Red,
-	};
+enum Color {
+  Black,
+  Red,
+};
 
-	class BSTNode {
-	public:
-		BSTNode* parent = nullptr;
-		BSTNode* link[2] = { nullptr,nullptr };
-		Data data;
-		bool color = Color::Black;
+class BSTNode {
+ public:
+  BSTNode* parent = nullptr;
+  BSTNode* link[2] = {nullptr, nullptr};
+  Data data;
+  bool color = Color::Black;
 
-		BSTNode() = default;
-		explicit BSTNode(const Data& value) : data(value) {
-			color = Color::Red;
-		};
-	};
+  BSTNode() = default;
+  explicit BSTNode(const Data& value) : data(value) { color = Color::Red; };
+};
 
-	class SelfBalancingBinarySearchTree :BaseClass
-	{
-	public:
-		class BSTIterator;
-		using Node = BSTNode;
-		using Pointer = Node*;
-		using Iterator = BSTIterator;
-		using ValueType = Data;
-		Pointer leaf_;
+class SelfBalancingBinarySearchTree : BaseClass {
+ public:
+  class BSTIterator;
+  using Node = BSTNode;
+  using Pointer = Node*;
+  using Iterator = BSTIterator;
+  using ValueType = Data;
+  Pointer leaf_;
 
-		SelfBalancingBinarySearchTree();
-		~SelfBalancingBinarySearchTree();
+  SelfBalancingBinarySearchTree();
+  ~SelfBalancingBinarySearchTree();
 
-		// internal class BSTIterator
-		class BSTIterator {
-			friend class SelfBalancingBinarySearchTree;
+  // internal class BSTIterator
+  class BSTIterator {
+    friend class SelfBalancingBinarySearchTree;
 
-		public:
-			BSTIterator(Pointer node, Pointer leaf) : current_(node), leaf_(leaf) {}
-			BSTIterator() : current_(nullptr), leaf_(nullptr) {}
-			BSTIterator(const BSTIterator& obj) {
-				current_ = obj.current_;
-				leaf_ = obj.leaf_;
-			}
+   public:
+    BSTIterator(Pointer node, Pointer leaf) : current_(node), leaf_(leaf) {}
+    BSTIterator() : current_(nullptr), leaf_(nullptr) {}
+    BSTIterator(const BSTIterator& obj) {
+      current_ = obj.current_;
+      leaf_ = obj.leaf_;
+    }
 
-			BSTIterator& operator=(const BSTIterator& obj) noexcept {
-				current_ = obj.current_;
-				leaf_ = obj.leaf_;
-				return *this;
-			}
+    BSTIterator& operator=(const BSTIterator& obj) noexcept {
+      current_ = obj.current_;
+      leaf_ = obj.leaf_;
+      return *this;
+    }
 
-			BSTIterator& operator--() noexcept {
-				if (current_->link[0] != leaf_) {
-					current_ = current_->link[0];
-					while (current_->link[1] != leaf_) {
-						current_ = current_->link[1];
-					}
-				}
-				else {
-					Pointer tmp = current_->parent;
-					while (tmp && tmp->data.GetKey() > current_->data.GetKey()) {
-						current_ = tmp;
-						tmp = tmp->parent;
-					}
-					current_ = tmp;
+    BSTIterator& operator--() noexcept {
+      if (current_->link[0] != leaf_) {
+        current_ = current_->link[0];
+        while (current_->link[1] != leaf_) {
+          current_ = current_->link[1];
+        }
+      } else {
+        Pointer tmp = current_->parent;
+        while (tmp && tmp->data.GetKey() > current_->data.GetKey()) {
+          current_ = tmp;
+          tmp = tmp->parent;
+        }
+        current_ = tmp;
+      }
+      return *this;
+    }
 
-				}
-				return *this;
-			}
+    BSTIterator& operator++() noexcept {
+      if (current_->link[1] != leaf_) {
+        current_ = current_->link[1];
+        while (current_->link[0] != leaf_) {
+          current_ = current_->link[0];
+        }
+      } else {
+        Pointer tmp = current_->parent;
+        while (tmp && tmp->data.GetKey() < current_->data.GetKey()) {
+          current_ = tmp;
+          tmp = tmp->parent;
+        }
+        current_ = tmp;
+      }
+      return *this;
+    }
 
-			BSTIterator& operator++() noexcept {
-				if (current_->link[1] != leaf_) {
-					current_ = current_->link[1];
-					while (current_->link[0] != leaf_) {
-						current_ = current_->link[0];
-					}
-				}
-				else {
-					Pointer tmp = current_->parent;
-					while (tmp && tmp->data.GetKey() < current_->data.GetKey()) {
-						current_ = tmp;
-						tmp = tmp->parent;
-					}
-					current_ = tmp;
-				}
-				return *this;
-			}
+    bool operator!=(const BSTIterator& other) const noexcept {
+      return (current_ != other.current_);
+    }
 
-			bool operator!=(const BSTIterator& other) const noexcept {
-				return (current_ != other.current_);
-			}
+    bool IsValid() const { return current_ != nullptr; }
 
-			bool IsValid() const {
-				return current_ != nullptr;
-			}
+    ValueType* operator->() noexcept { return &(current_->data); }
 
-			ValueType* operator->() noexcept { return &(current_->data); }
+   private:
+    Pointer current_;
+    Pointer leaf_;
+  };
 
-		private:
-			Pointer current_;
-			Pointer leaf_;
-		};
+  bool Set(std::string key, Student student, int validity = 0);
+  Student Get(std::string key);
+  bool Exists(std::string key);
+  bool Del(std::string key);
+  bool Update(std::string key, Student student);
+  std::vector<std::string> Keys();
+  bool Rename(std::string key, std::string new_key);
+  std::string Ttl(std::string param);
+  std::vector<std::string> Find(Student student);
+  std::vector<Student> Showall();
+  std::pair<bool, int> Upload(std::string path);
+  std::pair<bool, int> Export(std::string path);
 
-		bool Set(std::string key, Student student, int validity = 0);
-		Student Get(std::string key);
-		bool Exists(std::string key);
-		bool Del(std::string key);
-		bool Update(std::string key, Student student);
-		std::vector<std::string> Keys();
-		bool Rename(std::string key, std::string new_key);
-		std::string Ttl(std::string param);
-		std::vector<std::string> Find(Student student);
-		std::vector<Student> Showall();
-		std::pair<bool, int> Upload(std::string path);
-		std::pair<bool, int> Export(std::string path);
-
-
-	private:
-		Pointer root_;
-		void Swap(Pointer a, Pointer b);
-		void LeftRotation(Pointer node);
-		void RightRotation(Pointer node);
-		void BalanceTree(Pointer node);
-		Iterator Begin();
-		Iterator End();
-		void Clear();
-		int CountChildren(Pointer node) const;
-		Pointer GetChild(Pointer node) const;
-		Pointer GetParent(Pointer node) const;
-		Pointer Search(std::string key) const;
-		bool RemoveNode(Pointer node);
-		void RemoveNodeWithoutChildren(Pointer node);
-		void RemoveNodeWithOneChild(Pointer node);
-		void RemoveNodeWithTwoChild(Pointer node);
-		void RebalanceTree(Pointer node);
-		bool NodeIsLeftChild(Pointer node);
-		Pointer MinValueFromRight(const Pointer node) const;
-		void ExcludeNode(Pointer a, Pointer b);
-		bool CompareValue(Student student, Student search_template);
-		bool ParseLine(std::string line);
-	};
+ private:
+  Pointer root_;
+  void Swap(Pointer a, Pointer b);
+  void LeftRotation(Pointer node);
+  void RightRotation(Pointer node);
+  void BalanceTree(Pointer node);
+  Iterator Begin();
+  Iterator End();
+  void Clear();
+  int CountChildren(Pointer node) const;
+  Pointer GetChild(Pointer node) const;
+  Pointer GetParent(Pointer node) const;
+  Pointer Search(std::string key) const;
+  bool RemoveNode(Pointer node);
+  void RemoveNodeWithoutChildren(Pointer node);
+  void RemoveNodeWithOneChild(Pointer node);
+  void RemoveNodeWithTwoChild(Pointer node);
+  void RebalanceTree(Pointer node);
+  bool NodeIsLeftChild(Pointer node);
+  Pointer MinValueFromRight(const Pointer node) const;
+  void ExcludeNode(Pointer a, Pointer b);
+  bool CompareValue(Student student, Student search_template);
+  bool ParseLine(std::string line);
+};
 
 }  // namespace s21
 
--- src/common/data.h
+++ src/common/data.h
@@ -1,49 +1,50 @@
 #ifndef TRANSACTIONS_COMMON_DATA_H_
 #define TRANSACTIONS_COMMON_DATA_H_
-#include <string>
 #include <sstream>
-
+#include <string>
 
 namespace s21 {
-	struct Student {
-	public:
-		std::string name;
-		std::string surname;
-		int birthday{ 0 };
-		std::string city;
-		int coins{ 0 };
-		Student() = default;
-		Student(std::string name, std::string surname, int birthday, std::string city, int coins) {
-			this->name = name;
-			this->surname = surname;
-			this->birthday = birthday;
-			this->city = city;
-			this->coins = coins;
-		}
-		std::string ToString() {
-			std::stringstream ss;
-			ss << name << " " << surname << " " << std::to_string(birthday) << " " << city << " " << std::to_string(coins);
-			return ss.str();
-		}
-	};
+struct Student {
+ public:
+  std::string name;
+  std::string surname;
+  int birthday{0};
+  std::string city;
+  int coins{0};
+  Student() = default;
+  Student(std::string name, std::string surname, int birthday, std::string city,
+          int coins) {
+    this->name = name;
+    this->surname = surname;
+    this->birthday = birthday;
+    this->city = city;
+    this->coins = coins;
+  }
+  std::string ToString() {
+    std::stringstream ss;
+    ss << name << " " << surname << " " << std::to_string(birthday) << " "
+       << city << " " << std::to_string(coins);
+    return ss.str();
+  }
+};
 
-	class Data {
-	public:
-		Data() = default;
-		Data(std::string key, Student value, int validity = 0) : key_(key), value_(value), validity_(validity) {}
+class Data {
+ public:
+  Data() = default;
+  Data(std::string key, Student value, int validity = 0)
+      : key_(key), value_(value), validity_(validity) {}
 
-		std::string GetKey() { return key_; }
-		Student GetValue() { return value_; }
-		int GetValidity() { return validity_; }
-		void SetKey(std::string key) { key_ = key; }
-		void SetValue(Student value) { value_ = value; }
-		void SetValidity(int validity) { validity_ = validity; }
+  std::string GetKey() { return key_; }
+  Student GetValue() { return value_; }
+  int GetValidity() { return validity_; }
+  void SetKey(std::string key) { key_ = key; }
+  void SetValue(Student value) { value_ = value; }
+  void SetValidity(int validity) { validity_ = validity; }
 
-	private:
-		std::string key_;
-		Student value_;
-		int validity_{ 0 };
-
-	};
+ private:
+  std::string key_;
+  Student value_;
+  int validity_{0};
+};
 }  // namespace s21
 #endif  // TRANSACTIONS_COMMON_DATA_H_
