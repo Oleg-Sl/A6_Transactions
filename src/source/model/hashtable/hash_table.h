2d4dab087d5ee2553d84fae52a07c377fdbd8648
kvizord <kvizord@gmail.com>
2024-03-29 14:15:53 +0700

    Fix TTL

--- src/source/model/hashtable/hash_table.h
+++ src/source/model/hashtable/hash_table.h
@@ -11,61 +11,67 @@
 #include <vector>
 
 namespace s21 {
-const size_t kDefaultSize = 8;
 
 template <typename T, typename Hasher = std::hash<std::string>>
 class HashTable : BaseClass {
+ public:
+  using time_type = std::chrono::steady_clock::time_point;
+
+  static const size_t kDefaultSize = 8;
+
   struct Node {
     std::string key;
     T value;
-    int time_to_live;
-    std::chrono::steady_clock::time_point create_time;
+    int TTL;
+    time_type create_time;
+    bool valid = false;
+
+    Node(std::string key, T value, int TTL, time_type create_time)
+        : key(key),
+          value(value),
+          TTL(TTL),
+          create_time(create_time),
+          valid(true) {}
+
+    Node() {}
   };
 
- public:
   HashTable() {}
 
-  bool Set(std::string key, T value, int validity = 0) {
+  bool Set(std::string key, T value, int TTL = 0) {
     data_.at(GetIndex(key))
-        .push_back(
-            Node{key, value, validity, std::chrono::steady_clock::now()});
+        .push_back(Node{key, value, TTL, std::chrono::steady_clock::now()});
 
     return 0;
   }
 
   T Get(std::string key) {
-    auto response_time = std::chrono::steady_clock::now();
-
-    for (auto const& node : data_.at(GetIndex(key))) {
-      if (node.key == key && !CheckTTLExpired(response_time, node.create_time)) {
-        return node.value;
-      }
+    Node node = GetNode(key);
+    if (node.valid) {
+      return node.value;
     }
 
     throw std::invalid_argument("Key is not exists");
   }
 
-  bool Exists(std::string key) {
-    auto response_time = std::chrono::steady_clock::now();
-
-    for (auto const& node : data_.at(GetIndex(key))) {
-      if (node.key == key && !CheckTTLExpired(response_time, node.create_time)) {
-        return true;
-      }
-    }
-
-    return false;
-  }
+  bool Exists(std::string key) { return GetNode(key).valid; }
 
   bool Del(std::string key) {}
 
   bool Update(std::string key, T value) {}
+
   std::vector<std::string> Keys() {}
+
   bool Rename(std::string key, std::string new_key) {}
-  std::string Ttl(std::string param) {}
+
+  std::string Ttl(std::string key) {}
+
   std::vector<std::string> Find(T value) {}
+
   std::vector<T> Showall() {}
+
   std::pair<bool, int> Upload(std::string path) {}
+
   std::pair<bool, int> Export(std::string path) {}
 
  private:
@@ -75,14 +81,24 @@ class HashTable : BaseClass {
 
   size_t GetIndex(std::string value) { return hasher_(value) % table_size_; }
 
-  bool CheckTTLExpired(
-      const std::chrono::steady_clock::time_point& response_time,
-      const std::chrono::steady_clock::time_point& transaction_create_time) {
-    return std::chrono::duration_cast<std::chrono::seconds>(
-               transaction_create_time - response_time)
-                       .count() < 0
+  bool CheckTTLExpired(const time_type& response_time, const Node& element) {
+    return std::chrono::duration_cast<std::chrono::seconds>(response_time -
+                                                            element.create_time)
+                       .count() > element.TTL
                ? true
                : false;
+  }
+
+  Node GetNode(const std::string& key) {
+    auto response_time = std::chrono::steady_clock::now();
+
+    for (auto const& node : data_.at(GetIndex(key))) {
+      if (node.key == key && !CheckTTLExpired(response_time, node)) {
+        return node;
+      }
+    }
+
+    return Node();
   }
 };
 }  // namespace s21
