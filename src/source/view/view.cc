a9dc3adbcbdedfb40d7bfcd0e750ecf86793f7fc
kvizord <kvizord@gmail.com>
2024-04-10 18:21:52 +0700

    Rewrite view in progress

--- src/source/view/view.cc
+++ src/source/view/view.cc
@@ -20,274 +20,92 @@ void View::Start() {
   }
 }
 
-View::Vector View::GetParams(std::string str) {
-  Parser parser;
-  auto params = parser.Parse(str);
-  return params;
-}
+void View::Set(std::stringstream& stream) {
+  std::string key = parser_.ParseValue<std::string>(stream, "KEY");
+  Student student = parser_.ParseValue<Student>(stream, "STUDENT");
 
-std::string View::GetCommand(Vector params) { return ToLower(params[0]); }
-
-void View::Execute(std::string command, Vector params) {
-  std::vector<std::string> commands = {"set",    "get",     "exists", "del",
-                                       "update", "keys",    "rename", "ttl",
-                                       "find",   "showall", "upload", "export"};
-  int ind = -1;
-  for (int i = 0; i < commands.size(); ++i) {
-    if (command == commands[i]) {
-      ind = i;
-      break;
-    }
-  }
-
-  switch (ind) {
-    case 0:
-      CallSet(params);
-      break;
-    case 1:
-      CallGet(params);
-      break;
-    case 2:
-      CallExists(params);
-      break;
-    case 3:
-      CallDel(params);
-      break;
-    case 4:
-      CallUpdate(params);
-      break;
-    case 5:
-      CallKeys();
-      break;
-    case 6:
-      CallRename(params);
-      break;
-    case 7:
-      CallTtl(params);
-      break;
-    case 8:
-      CallFind(params);
-      break;
-    case 9:
-      CallShowall();
-      break;
-    case 10:
-      CallUpload(params);
-      break;
-    case 11:
-      CallExport(params);
-      break;
-    default:
-      break;
-  }
-}
-
-void View::CallSet(Vector params) {
-  bool res = true;
-  if (params.size() != 7 && params.size() != 9) {
-    IncorrectParamsMessage();
-    return;
-  }
-
-  if (params.size() == 7) {
-    auto birth = ParseToInt(params[4]);
-    if (!birth.first) return;
-    auto coins = ParseToInt(params[6]);
-    if (!coins.first) return;
-    res = controller_->Set(
-        params[1],
-        Student(params[2], params[3], birth.second, params[5], coins.second));
-  }
-
-  if (params.size() == 9) {
-    auto birth = ParseToInt(params[4]);
-    if (!birth.first) return;
-    auto coins = ParseToInt(params[6]);
-    if (!coins.first) return;
-    if (ToLower(params[7]) != "ex") {
-      IncorrectParamsMessage();
-      return;
-    }
-    auto validity = ParseToInt(params[8]);
-    if (!validity.first) return;
-    res = controller_->Set(
-        params[1],
-        Student(params[2], params[3], birth.second, params[5], coins.second),
-        validity.second);
-  }
-
-  if (res) {
-    std::cout << "OK" << std::endl;
+  auto optional_arg = parser_.ParseOptionalArgument<int>(stream, "EX");
+  if (optional_arg.first.empty()) {
+    controller_->Set(key, student);
   } else {
-    std::cout << "This key already exists" << std::endl;
+    controller_->Set(key, student, optional_arg.second);
   }
 }
 
-void View::CallGet(Vector params) {
-  if (params.size() != 2) {
-    IncorrectParamsMessage();
-    return;
-  }
-  auto res = controller_->Get(params[1]);
-
-  if (res.IsEmpty()) {
+void View::Get(std::stringstream& stream) {
+  std::string key = parser_.ParseValue<std::string>(stream, "KEY");
+  try {
+    Student student = controller_->Get(key);
+    std::cout << student << std::endl;
+  } catch (std::invalid_argument& ex) {
     std::cout << "(null)" << std::endl;
-  } else {
-    std::cout << res.ToString() << std::endl;
   }
 }
 
-void View::CallExists(Vector params) {
-  if (params.size() != 2) {
-    IncorrectParamsMessage();
-    return;
-  }
-  auto res = controller_->Exists(params[1]);
-  if (res) {
-    std::cout << "true" << std::endl;
-  } else {
-    std::cout << "false" << std::endl;
+void View::Exists(std::stringstream& stream) {
+  std::string key = parser_.ParseValue<std::string>(stream, "KEY");
+  std::cout << controller_->Exists(key) << std::endl;
+}
+
+void View::Del(std::stringstream& stream) {
+  std::string key = parser_.ParseValue<std::string>(stream, "KEY");
+  std::cout << controller_->Del(key) << std::endl;
+}
+
+void View::Update(std::stringstream& stream) {
+  std::string key = parser_.ParseValue<std::string>(stream, "KEY");
+  Student student = parser_.ParseValue<Student>(stream, "Student");
+  std::cout << controller_->Update(key, student) << std::endl;
+}
+
+void View::Keys(std::stringstream& stream) {
+  size_t counter = 1;
+  for (auto& key : controller_->Keys()) {
+    std::cout << counter << ") " << key << std::endl;
+    ++counter;
   }
 }
 
-void View::CallDel(Vector params) {
-  if (params.size() != 2) {
-    IncorrectParamsMessage();
-    return;
-  }
-  auto res = controller_->Del(params[1]);
-  if (res) {
-    std::cout << "true" << std::endl;
-  } else {
-    std::cout << "false" << std::endl;
+void View::Rename(std::stringstream& stream) {
+  std::string key_1 = parser_.ParseValue<std::string>(stream, "KEY1");
+  std::string key_2 = parser_.ParseValue<std::string>(stream, "KEY2");
+  std::cout << controller_->Rename(key_1, key_2) << std::endl;
+}
+
+void View::Ttl(std::stringstream& stream) {
+  try {
+    std::string key = parser_.ParseValue<std::string>(stream, "KEY");
+    std::cout << controller_->Ttl(key) << std::endl;
+  } catch (std::invalid_argument& ex) {
+    std::cout << "(null)" << std::endl;
   }
 }
 
-void View::CallUpdate(Vector params) {
-  if (params.size() != 7) {
-    IncorrectParamsMessage();
-    return;
-  }
-
-  int birthday;
-  if (params[4] == "-") {
-    birthday = INT32_MAX;
-  } else {
-    auto res = ParseToInt(params[4]);
-    if (!res.first) return;
-    birthday = res.second;
-  }
-
-  int coins;
-  if (params[6] == "-") {
-    coins = INT32_MAX;
-  } else {
-    auto res = ParseToInt(params[6]);
-    if (!res.first) return;
-    coins = res.second;
-  }
-
-  bool result = controller_->Update(
-      params[1], Student(params[2], params[3], birthday, params[5], coins));
-
-  if (result) {
-    std::cout << "OK" << std::endl;
-  } else {
-    std::cout << "Update error" << std::endl;
+void View::Find(std::stringstream& stream) {
+  Student student = parser_.ParseValue<Student>(stream, "STUDENT");
+  size_t counter = 1;
+  for (auto& key : controller_->Find(student)) {
+    std::cout << counter << ") " << key << std::endl;
+    ++counter;
   }
 }
 
-void View::CallKeys() {
-  auto keys = controller_->Keys();
-  for (size_t i = 0; i < keys.size(); ++i) {
-    std::cout << i + 1 << ") " << keys[i] << std::endl;
+void View::Showall(std::stringstream& stream) {
+  size_t counter = 1;
+  for (auto& student : controller_->Showall()) {
+    std::cout << counter << ") " << student << std::endl;
+    ++counter;
   }
 }
 
-void View::CallRename(Vector params) {
-  if (params.size() != 3) {
-    IncorrectParamsMessage();
-    return;
-  }
-  bool res = controller_->Rename(params[1], params[2]);
-  if (res) {
-    std::cout << "OK" << std::endl;
-  } else {
-    std::cout << "Rename error" << std::endl;
-  }
+void View::Upload(std::stringstream& stream) {
+  std::string path = parser_.ParseValue<std::string>(stream, "path");
+  controller_->Upload(path);
 }
 
-void View::CallTtl(Vector params) {}
-
-void View::CallFind(Vector params) {
-  if (params.size() != 6) {
-    IncorrectParamsMessage();
-    return;
-  }
-
-  int birthday;
-  if (params[3] == "-") {
-    birthday = INT32_MAX;
-  } else {
-    auto res = ParseToInt(params[3]);
-    if (!res.first) return;
-    birthday = res.second;
-  }
-
-  int coins;
-  if (params[5] == "-") {
-    coins = INT32_MAX;
-  } else {
-    auto res = ParseToInt(params[5]);
-    if (!res.first) return;
-    coins = res.second;
-  }
-
-  auto keys = controller_->Find(
-      Student(params[1], params[2], birthday, params[4], coins));
-
-  for (size_t i = 0; i < keys.size(); ++i) {
-    std::cout << i + 1 << ") " << keys[i] << std::endl;
-  }
-}
-
-void View::CallShowall() {
-  auto students = controller_->Showall();
-  if (students.size() != 0) {
-    std::cout << std::left << std::setw(3) << "#" << std::left << std::setw(15)
-              << "Name" << std::left << std::setw(15) << "Surname" << std::left
-              << std::setw(10) << "Birthday" << std::left << std::setw(15)
-              << "City" << std::left << std::setw(8) << "Coins" << std::endl;
-    for (size_t i = 0; i < students.size(); ++i) {
-      std::cout << std::setw(3) << i + 1 << students[i].ToString() << std::endl;
-    }
-  }
-}
-
-void View::CallUpload(Vector params) {
-  if (params.size() != 2) {
-    IncorrectParamsMessage();
-    return;
-  }
-  auto res = controller_->Upload(params[1]);
-  if (res.first) {
-    std::cout << "OK " << res.second << std::endl;
-  } else {
-    std::cout << "Upload error" << std::endl;
-  }
-}
-
-void View::CallExport(Vector params) {
-  if (params.size() != 2) {
-    IncorrectParamsMessage();
-    return;
-  }
-  auto res = controller_->Export(params[1]);
-  if (res.first) {
-    std::cout << "OK " << res.second << std::endl;
-  } else {
-    std::cout << "Export error" << std::endl;
-  }
+void View::Export(std::stringstream& stream) {
+  std::string path = parser_.ParseValue<std::string>(stream, "path");
+  controller_->Export(path);
 }
 
 void View::InputCommandAndParams() {
@@ -297,12 +115,19 @@ void View::InputCommandAndParams() {
     std::string input;
     getline(std::cin, input);
 
-    auto params = GetParams(input);
-    std::string command = GetCommand(params);
-    if (command == "quit")
-      break;
-    else
-      Execute(command, params);
+    std::stringstream stream(input);
+    std::string command = parser_.ParseValue<std::string>(stream, "COMMAND");
+
+    try {
+      auto callable = kCommands.find(command);
+      if (callable != kCommands.end()) {
+        callable->second(stream);
+      } else {
+        std::cout << "INCORRECT COMMAND " << command << std::endl;
+      }
+    } catch (std::exception& ex) {
+      std::cout << ex.what() << std::endl;
+    }
   }
 }
 
@@ -314,25 +139,4 @@ void View::ShowMainMenu() {
   std::cout << "4 (Exit)" << std::endl;
 }
 
-void View::IncorrectParamsMessage() {
-  std::cout << "Incorrect params entered. Try again" << std::endl;
-}
-
-std::pair<bool, int> View::ParseToInt(std::string str) {
-  try {
-    int res = stoi(str);
-    return std::make_pair(true, res);
-  } catch (...) {
-    std::cout << "ERROR: unable to cast value " << str << " to type int"
-              << std::endl;
-    return std::make_pair(false, 0);
-  }
-}
-
-std::string View::ToLower(std::string str) {
-  for (auto& c : str) {
-    c = tolower(c);
-  }
-  return str;
-}
 }  // namespace s21
--- src/source/view/view.h
+++ src/source/view/view.h
@@ -1,36 +1,55 @@
 #ifndef TRANSACTIONS_VIEW_VIEW_H_
 #define TRANSACTIONS_VIEW_VIEW_H_
+#include <functional>
 #include <iomanip>
 #include <iostream>
 #include <limits>
+#include <set>
 #include <sstream>
 #include <vector>
 
 #include "controller/controller.h"
+#include "model/parser/parser.h"
 
 namespace s21 {
 class View {
  public:
   using Vector = std::vector<std::string>;
+
+  const std::unordered_map<std::string, std::function<void(std::stringstream&)>>
+      kCommands = {
+          {"set", [this](std::stringstream& stream) { Set(stream); }},
+          {"get", [this](std::stringstream& stream) { Get(stream); }},
+          {"exists", [this](std::stringstream& stream) { Exists(stream); }},
+          {"del", [this](std::stringstream& stream) { Del(stream); }},
+          {"update", [this](std::stringstream& stream) { Update(stream); }},
+          {"keys", [this](std::stringstream& stream) { Keys(stream); }},
+          {"rename", [this](std::stringstream& stream) { Rename(stream); }},
+          {"ttl", [this](std::stringstream& stream) { Ttl(stream); }},
+          {"find", [this](std::stringstream& stream) { Find(stream); }},
+          {"showall", [this](std::stringstream& stream) { Showall(stream); }},
+          {"upload", [this](std::stringstream& stream) { Upload(stream); }},
+          {"export", [this](std::stringstream& stream) { Export(stream); }}};
+
   void Start();
 
  private:
   std::unique_ptr<Controller> controller_;
-  Vector GetParams(std::string str);
-  std::string GetCommand(Vector params);
-  void Execute(std::string command, Vector params);
-  void CallSet(Vector params);
-  void CallGet(Vector params);
-  void CallExists(Vector params);
-  void CallDel(Vector params);
-  void CallUpdate(Vector params);
-  void CallKeys();
-  void CallRename(Vector params);
-  void CallTtl(Vector params);
-  void CallFind(Vector params);
-  void CallShowall();
-  void CallUpload(Vector params);
-  void CallExport(Vector params);
+  Parser parser_;
+
+  void Set(std::stringstream& stream);
+  void Get(std::stringstream& stream);
+  void Exists(std::stringstream& stream);
+  void Del(std::stringstream& stream);
+  void Update(std::stringstream& stream);
+  void Keys(std::stringstream& stream);
+  void Rename(std::stringstream& stream);
+  void Ttl(std::stringstream& stream);
+  void Find(std::stringstream& stream);
+  void Showall(std::stringstream& stream);
+  void Upload(std::stringstream& stream);
+  void Export(std::stringstream& stream);
+
   void InputCommandAndParams();
   void ShowMainMenu();
   void IncorrectParamsMessage();
