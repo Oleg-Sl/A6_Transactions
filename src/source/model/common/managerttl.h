7ce3315a197c5649d49ca06d38b61bf8e0831717
kvizord <kvizord@gmail.com>
2024-04-24 20:19:49 +0700

    Fix ManagerTTL

--- src/source/model/common/managerttl.h
+++ src/source/model/common/managerttl.h
@@ -1,13 +1,13 @@
 #ifndef TRANSACTIONS_SOURCE_MODEL_COMMON_MANAGERTTL_H_
 #define TRANSACTIONS_SOURCE_MODEL_COMMON_MANAGERTTL_H_
 
+#include <atomic>
 #include <chrono>
 #include <condition_variable>
-#include <iostream>
+#include <functional>
 #include <mutex>
 #include <queue>
 #include <thread>
-#include <functional>
 
 #include "model/common/base_class.h"
 
@@ -27,19 +27,26 @@ class ManagerTTL {
     }
   };
 
-  ManagerTTL(BaseClass<Key, Value>& storage)
-      : storage_(storage),
-        thread_([this] { StartManager(std::chrono::seconds(5)); }) {}
+  ManagerTTL(BaseClass<Key, Value>& storage,
+             std::chrono::seconds clean_interaval)
+      : storage_(storage), manager_thread_([this, &clean_interaval] {
+          StartManagerLoop(clean_interaval);
+        }) {}
 
   ~ManagerTTL() {
     StopManager();
-    
-    thread_.join();
+    loop_condition_.notify_all();
+    manager_thread_.join();
   }
 
-  void AddRecord(const Record& record) { pq_.push(record); }
+  void AddRecord(const Record& record) {
+    std::unique_lock lock(pq_mtx_);
+    pq_.push(record);
+  }
 
   void DeleteExpiredRecords() {
+    std::unique_lock lock(storage_mtx_);
+    std::unique_lock lock2(pq_mtx_);
     std::chrono::steady_clock::time_point now =
         std::chrono::steady_clock::now();
     while (!pq_.empty() &&
@@ -49,48 +56,50 @@ class ManagerTTL {
     }
   }
 
-  void StartManager(std::chrono::seconds sleep_time) {
-    while (!stop_manager_) {
-      {
-        std::unique_lock<std::mutex> lock(mtx_);
-        cv_.wait_for(lock, sleep_time, [this] { return storage_access_; });
-        cv_.wait(lock, [this] { return storage_access_; });
-        storage_access_ = false;
-        DeleteExpiredRecords();
-        storage_access_ = true;
-        cv_.notify_all();
+  void DeleteRecord(const Key& key) {
+    std::unique_lock lock_2(pq_mtx_);
+    std::priority_queue<Record> temp;
+
+    while (!pq_.empty()) {
+      if (pq_.top().key != key) {
+        temp.push(pq_.top());
       }
+      pq_.pop();
+    }
+
+    std::swap(pq_, temp);
+  }
+
+  void StartManagerLoop(std::chrono::seconds sleep_time) {
+    while (!stop_manager_.load()) {
+      DeleteExpiredRecords();
+
+      std::mutex temp;
+      std::unique_lock lock(temp);
+      loop_condition_.wait_for(lock, sleep_time);
     }
   }
 
-  void StopManager() { stop_manager_ = true; }
+  void StopManager() { stop_manager_.store(true); }
 
   template <typename Func, typename... Args>
-  auto Execute(Func func, Args... args) {
-    {  // Delete expired records
-      std::unique_lock<std::mutex> lock(mtx_);
-      cv_.wait(lock, [this] { return storage_access_; });
-    }
-    cv_.notify_all();
-
-    std::unique_lock<std::mutex> lock(mtx_);
-    cv_.wait(lock, [this] { return storage_access_; });
-    storage_access_ = false;
+  auto ExecuteStorageOperation(Func func, Args... args) {
+    DeleteExpiredRecords();
+    std::unique_lock lock(storage_mtx_);
     auto result = std::invoke(func, storage_, std::forward<Args>(args)...);
-    storage_access_ = true;
-    cv_.notify_all();
+
     return result;
   }
 
  private:
   BaseClass<Key, Value>& storage_;
-  
-  std::mutex mtx_;
-  std::condition_variable cv_;
-  bool storage_access_ = true;
-  bool stop_manager_ = false;
   std::priority_queue<Record> pq_;
-  std::thread thread_;
+
+  std::atomic<bool> stop_manager_ = false;
+  std::condition_variable loop_condition_;
+  std::mutex storage_mtx_;
+  std::mutex pq_mtx_;
+  std::thread manager_thread_;
 };
 
 }  // namespace s21
