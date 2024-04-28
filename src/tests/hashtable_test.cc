58ec4987f59d37902e927be84c00bc9129d4101d
kvizord <kvizord@gmail.com>
2024-04-28 16:04:51 +0700

    Add tests

--- src/tests/hashtable_test.cc
+++ src/tests/hashtable_test.cc
@@ -1,22 +1,15 @@
-#include <chrono>
-#include <filesystem>
-#include <thread>
-
 #include "common.h"
 #include "model/common/student.h"
 #include "model/hashtable/hash_table.h"
 
 namespace s21 {
 
-static constexpr bool kOk = true;
-static constexpr bool kError = false;
-
 TEST(HashTableSet, OneElement) {
   HashTable<std::string, Student> table;
 
   int status = table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555});
 
-  ASSERT_EQ(status, kOk);
+  ASSERT_EQ(status, true);
   ASSERT_EQ(table.GetLoadFactor(), 1);
 }
 
@@ -27,7 +20,7 @@ TEST(HashTableSet, ManyElement) {
   for (size_t i = 0; i < count_elements; ++i) {
     int status = table.Set("KEY" + std::to_string(i),
                            Student{"NAME", "SURNAME", 12, "CITY", 5555});
-    ASSERT_EQ(status, kOk);
+    ASSERT_EQ(status, true);
   }
 
   ASSERT_EQ(table.GetLoadFactor(), count_elements);
@@ -41,26 +34,10 @@ TEST(HashTableSet, ElementsWithSameKey) {
   int status_2 =
       table.Set("KEY", Student{"NAME2", "SURNAME2", 12, "CITY", 5555});
 
-  ASSERT_EQ(status_1, kOk);
-  ASSERT_EQ(status_2, kError);
+  ASSERT_EQ(status_1, true);
+  ASSERT_EQ(status_2, false);
   ASSERT_EQ(table.GetLoadFactor(), 1);
 }
-
-// TEST(HashTableSet, ElementsWithSameKeyAndTTLExpired) {
-//   HashTable<std::string, Student> table;
-
-//   int status_1 =
-//       table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 1);
-
-//   std::this_thread::sleep_for(std::chrono::milliseconds(1001));
-
-//   int status_2 =
-//       table.Set("KEY", Student{"NAME2", "SURNAME2", 12, "CITY", 5555});
-
-//   ASSERT_EQ(status_1, kOk);
-//   ASSERT_EQ(status_2, kOk);
-//   ASSERT_EQ(table.GetLoadFactor(), 2);
-// }
 
 TEST(HashTableGet, Normal) {
   HashTable<std::string, Student> table;
@@ -93,26 +70,6 @@ TEST(HashTableExists, NotExistsKey) {
   ASSERT_EQ(status, false);
 }
 
-// TEST(HashTableExists, ExistsWithTTL) {
-//   HashTable<std::string, Student> table;
-//   table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 5);
-
-//   bool status = table.Exists("KEY");
-
-//   ASSERT_EQ(status, true);
-// }
-
-// TEST(HashTableExists, NotExistsTTLExpired) {
-//   HashTable<std::string, Student> table;
-//   table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 1);
-
-//   std::this_thread::sleep_for(std::chrono::milliseconds(1001));
-
-//   bool status = table.Exists("KEY");
-
-//   ASSERT_EQ(status, false);
-// }
-
 TEST(HashTableUpdate, Success) {
   HashTable<std::string, Student> table;
   Student student{"NAME", "SURNAME", 12, "CITY", 5555};
@@ -134,18 +91,6 @@ TEST(HashTableUpdate, FailKeyIsNotExists) {
   ASSERT_EQ(status, false);
 }
 
-// TEST(HashTableUpdate, FailTTLExpired) {
-//   HashTable<std::string, Student> table;
-//   table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 1);
-
-//   std::this_thread::sleep_for(std::chrono::milliseconds(1001));
-
-//   bool status =
-//       table.Update("KEY", Student{"NAME2", "SURNAME2", 13, "CITY2", 5556});
-
-//   ASSERT_EQ(status, false);
-// }
-
 TEST(HashTableKeys, Normal) {
   HashTable<std::string, Student> table;
   std::set<std::string> keys = {"KEY", "KEY2", "KEY3"};
@@ -160,23 +105,6 @@ TEST(HashTableKeys, Normal) {
   ASSERT_EQ(keys.size(), result.size());
   ASSERT_EQ(result_set, keys);
 }
-
-// TEST(HashTableKeys, KeyWithTTLExpired) {
-//   HashTable<std::string, Student> table;
-//   std::set<std::string> keys = {"KEY", "KEY2", "KEY3"};
-
-//   for (auto key : keys) {
-//     table.Set(key, Student{"NAME", "SURNAME", 12, "CITY", 5555});
-//   }
-//   table.Set("KEY4", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 1);
-//   std::this_thread::sleep_for(std::chrono::milliseconds(1001));
-
-//   std::vector<std::string> result = table.Keys();
-//   std::set<std::string> result_set(result.begin(), result.end());
-
-//   ASSERT_EQ(keys.size(), result.size());
-//   ASSERT_EQ(result_set, keys);
-// }
 
 TEST(HashTableRename, Normal) {
   HashTable<std::string, Student> table;
@@ -199,18 +127,6 @@ TEST(HashTableRename, InvalidKey) {
   ASSERT_EQ(status, false);
 }
 
-// TEST(HashTableRename, KeyWithTTLExpired) {
-//   HashTable<std::string, Student> table;
-//   table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 1);
-//   std::this_thread::sleep_for(std::chrono::milliseconds(1001));
-
-//   bool status = table.Rename("KEY", "KEY2");
-
-//   ASSERT_EQ(table.Exists("KEY"), false);
-//   ASSERT_EQ(table.Exists("KEY2"), false);
-//   ASSERT_EQ(status, false);
-// }
-
 TEST(HashTableDel, Normal) {
   HashTable<std::string, Student> table;
   table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555});
@@ -227,86 +143,6 @@ TEST(HashTableDel, InvalidKey) {
   bool status = table.Del("KEY");
 
   ASSERT_EQ(status, false);
-}
-
-// TEST(HashTableDel, DelWithTTLExpired) {
-//   HashTable<std::string, Student> table;
-//   table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 1);
-
-//   std::this_thread::sleep_for(std::chrono::milliseconds(1001));
-//   bool status = table.Del("KEY");
-
-//   ASSERT_EQ(status, false);
-//   ASSERT_EQ(table.Exists("KEY"), false);
-// }
-
-// TEST(HashTableTTL, Normal) {
-//   HashTable<std::string, Student> table;
-//   table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 1);
-
-//   int res = table.Ttl("KEY");
-
-//   ASSERT_EQ(res, 1);
-// }
-
-// TEST(HashTableTTL, ThrowKeyWithTTLExpired) {
-//   HashTable<std::string, Student> table;
-//   table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555}, 1);
-
-//   std::this_thread::sleep_for(std::chrono::milliseconds(1001));
-
-//   ASSERT_THROW(table.Ttl("KEY"), std::invalid_argument);
-// }
-
-// TEST(HashTableTTL, ThrowInvalidKey) {
-//   HashTable<std::string, Student> table;
-
-//   ASSERT_THROW(table.Ttl("KEY"), std::invalid_argument);
-// }
-
-TEST(HashTableExport, Normal) {
-  HashTable<std::string, Student> table;
-  table.Set("KEY", Student{"NAME", "SURNAME", 12, "CITY", 5555});
-  table.Set("KEY2", Student{"NAME", "SURNAME", 12, "CITY", 5555});
-  table.Set("KEY3", Student{"NAME", "SURNAME", 12, "CITY", 5555});
-  std::string temp_file =
-      (std::filesystem::temp_directory_path() / "temp.dat").string();
-
-  table.Export(temp_file);
-
-  ASSERT_EQ(std::fstream(temp_file).is_open(), true);
-  std::remove(temp_file.c_str());
-  ASSERT_EQ(std::fstream(temp_file).is_open(), false);
-}
-
-TEST(HashTableUpload, Normal) {
-  HashTable<std::string, Student> table;
-
-  auto result = table.Upload(kSamplesDir + "student.dat");
-
-  ASSERT_EQ(result.first, true);
-  ASSERT_EQ(result.second, 20);
-  ASSERT_EQ(table.GetLoadFactor(), 20);
-}
-
-TEST(HashTableUpload, FileNotExists) {
-  HashTable<std::string, Student> table;
-
-  auto result = table.Upload(kSamplesDir + "notexists.dat");
-
-  ASSERT_EQ(result.first, false);
-  ASSERT_EQ(result.second, 0);
-  ASSERT_EQ(table.GetLoadFactor(), 0);
-}
-
-TEST(HashTableUpload, FileIncorrectStucture) {
-  HashTable<std::string, Student> table;
-
-  auto result = table.Upload(kSamplesDir + "student_incorrect.dat");
-
-  ASSERT_EQ(result.first, true);
-  ASSERT_EQ(result.second, 0);
-  ASSERT_EQ(table.GetLoadFactor(), 0);
 }
 
 TEST(HashTableFind, OneElements) {
